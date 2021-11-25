7e1b22b7e0619a5859faa86ee13a2d542154689c
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2021-11-25 17:10:12 +0300

    Change settings

--- backend/asgi.py
+++ backend/asgi.py
@@ -12,7 +12,6 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from whitenoise.django import DjangoWhiteNoise
 
 from auction.routing import websocket_urlpatterns
 
@@ -24,4 +23,3 @@ application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
 
-application = DjangoWhiteNoise(application)
