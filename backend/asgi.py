77732816e91be8b7e18391c1e72ed8f0e905ab1d
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2021-11-25 15:36:21 +0300

    Prepare for deployment to Heroku

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -16,6 +16,7 @@ from channels.auth import AuthMiddlewareStack
 from auction.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
