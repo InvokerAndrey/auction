e2e2ac689ca2c0e3ca0dd10e5e22e9c3d720901c
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2021-11-29 14:23:05 +0300

    Remove STATICFILES_STORAGE

--- backend/production_settings.py
+++ backend/production_settings.py
@@ -176,7 +176,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
 
