03a202e6aaea1416375bf920e8b65b2ddb046f8a
ANDREI DYDYSHKA <dydyshko1999@gmail.com>
2021-12-01 11:27:27 +0300

    staticfiles

--- backend/production_settings.py
+++ backend/production_settings.py
@@ -171,11 +171,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
